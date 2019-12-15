50c4bf0598dcceb07133d703653f6a107f9539df
gbenznyc <gbenz@bowdoin.edu>
2019-12-15 18:35:00 -0500

    Tops off multiple_perceptrons

--- multiple_perceptrons.py
+++ multiple_perceptrons.py
@@ -29,10 +29,13 @@ class MultiplePerceptrons:
 		#Train the NN based on the amount of epochs
 		for i in range(self.epochs):
 			for data in training_data:
-				#Find the max value node 
-				prediction = self.predict(data["data"])
-				if prediction == data["answer"]:
-					error = 1
-				else:
-					error = 0
+				for perceptron in self.perceptrons:
+					#Find the max value node 
+					prediction = self.predict(data["data"])
+					if prediction == data["answer"]:
+						perceptron.learn(None, error=1)
+					else:
+						perceptron.learn(None, error=0)
+
+
 		
