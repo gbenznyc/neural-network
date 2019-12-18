47c516ca6d7e77540adf00cb66b64b70f2c824d8
tomjones-4 <tdjones@bowdoin.edu>
2019-12-18 16:17:57 -0500

    Testing

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ def main():
 	test_32 = Reader("digit-recognition-examples/32x32-bitmaps/optdigits-32x32.tes").return_data()
 	test_8 = Reader("digit-recognition-examples/8x8-integer-inputs/optdigits-8x8-int.tes").return_data()
 
+	"""
 	gn = MultiplePerceptrons(64, 0.1, 5)
 	gn.learn(train_8)
 	correct = 0
@@ -18,18 +19,18 @@ def main():
 			correct += 1
 
 	print(correct/len(test_8))
-
+	"""
 	
-	# nn = Perceptron(1024, 0.1, 10, one_output=True)
-	# nn.learn(train_32)
-	# correct = 0
-	# for data in test_32:
-	# 	#print("data[answer]: ") #debug
-	# 	#print(data["answer"]) #debug
-	# 	print("nn.predict(data[data]): ") #debug
-	# 	print(nn.predict(data["data"])) #debug
-	# 	if nn.predict(data["data"]) == data["answer"]:
-	# 		correct += 1
+	nn = Perceptron(1024, 0.1, 10, one_output=True)
+	nn.learn(train_32)
+	correct = 0
+	for data in test_32:
+	 	#print("data[answer]: ") #debug
+	 	#print(data["answer"]) #debug
+	 	print("nn.predict(data[data]): ") #debug
+	 	print(nn.predict(data["data"])) #debug
+	 	if nn.predict(data["data"]) == data["answer"]:
+	 		correct += 1
 
 
 main()
