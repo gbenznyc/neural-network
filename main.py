46de978cf55505ca95c971daacab6e8a6ea4c493
tomjones-4 <tdjones@bowdoin.edu>
2019-12-18 16:12:15 -0500

    Working on single perceptron

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ def main():
 	test_32 = Reader("digit-recognition-examples/32x32-bitmaps/optdigits-32x32.tes").return_data()
 	test_8 = Reader("digit-recognition-examples/8x8-integer-inputs/optdigits-8x8-int.tes").return_data()
 
+	"""
 	gn = MultiplePerceptrons(64, 0.1, 1)
 	#gn.learn(train_8)
 	correct = 0
@@ -22,10 +23,22 @@ def main():
 			correct += 1
 
 	print(correct/len(test_8))
-
-
-	# nn = Perceptron(64, 0.1, 5, True)
-	# nn.learn(train_8)
+	"""
+	
+	nn = Perceptron(1024, 0.1, 10, one_output=True)
+	nn.learn(train_32)
+	correct = 0
+	for data in test_32:
+		#print("data[answer]: ") #debug
+		#print(data["answer"]) #debug
+		print("nn.predict(data[data]): ") #debug
+		print(nn.predict(data["data"])) #debug
+		if nn.predict(data["data"]) == data["answer"]:
+			correct += 1
+	
+	
+	#print(correct/len(test_32))
+	
 	# print("TEST")
 	# print(train_8[3]["answer"])
 	# print(nn.predict(train_8[1]["data"]))
