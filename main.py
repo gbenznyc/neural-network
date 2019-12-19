c67406f4c505259a520a7bf0610e6df93fd06330
gbenznyc <gbenz@bowdoin.edu>
2019-12-19 14:54:47 -0500

    Fixes code so 1024 works

--- main.py
+++ main.py
@@ -9,26 +9,10 @@ def main():
 	test_32 = Reader("digit-recognition-examples/32x32-bitmaps/optdigits-32x32.tes").return_data()
 	test_8 = Reader("digit-recognition-examples/8x8-integer-inputs/optdigits-8x8-int.tes").return_data()
 
-	gn = MultiplePerceptrons(64, 0.1, 25)
-	gn.learn(train_8, test_8)
-	correct = 0
-	for data in test_8:
-		if gn.predict(data["data"])[0] == data["answer"]:
-			correct += 1
+	print(train_8[0])
 
-	print(correct/len(test_8))
-
+	gn = MultiplePerceptrons(1024, 0.1, 25)
+	gn.learn(train_32, test_32)
 	
-	# nn = Perceptron(1024, 0.1, 10, one_output=True)
-	# nn.learn(train_32)
-	# correct = 0
-	# for data in test_32:
-	# 	#print("data[answer]: ") #debug
-	# 	#print(data["answer"]) #debug
-	# 	print("nn.predict(data[data]): ") #debug
-	# 	print(nn.predict(data["data"])) #debug
-	# 	if nn.predict(data["data"]) == data["answer"]:
-	# 		correct += 1
-
 
 main()
